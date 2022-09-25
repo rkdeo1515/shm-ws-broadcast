39e084f9b4e22854ad895a1ee2e28960289b35fa
jgd <rkdeo1515@gmail.com>
2022-09-25 20:45:38 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 mkdir ./bin
 gcc -o ./bin/write src/write.c src/ipc.c
 gcc -o ./bin/read src/read.c src/ipc.c
-gcc -o ./bin/bc.c src/bc src/ipc.c -I ./ws/usr/local/include/wsserver -L ./ws/usr/local/lib64/ -lws
+gcc -o ./bin/bc src/bc.c src/ipc.c -I ./ws/usr/local/include/wsserver -L ./ws/usr/local/lib64/ -lws
